69b90e43ae11adb5eabdb360e1309ce45dce14f0
hyunji1205 <sguswl1205@gmail.com>
2024-06-05 14:38:04 +0900

    room.html form check

--- src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/entity/ChatRoom.java
+++ src/main/java/com/ll/chat_2024_06_03/domain/chat/chatRoom/entity/ChatRoom.java
@@ -1,51 +1,40 @@
 package com.ll.chat_2024_06_03.domain.chat.chatRoom.entity;
 
-import jakarta.persistence.*;
+import com.ll.chat_2024_06_03.global.jpa.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
-@Builder
-@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor(access = PROTECTED)
 @NoArgsConstructor(access = PROTECTED)
-public class ChatRoom {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Getter
-    private Long id;
-
-    @CreatedDate
-    @Getter
-    private LocalDateTime createDate;
-
-    @LastModifiedDate
-    @Getter
-    private LocalDateTime modifyDate;
-
+@SuperBuilder
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class ChatRoom extends BaseEntity {
     @Getter
     private String name;
 
     @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     @Getter
-    private List<com.ll.chat_2024_06_03.domain.chat.chatRoom.entity.ChatMessage> chatMessages = new ArrayList<>();
+    @ToString.Exclude
+    private List<ChatMessage> chatMessages = new ArrayList<>();
 
     public ChatRoom(String name) {
         this.name = name;
     }
 
     public void writeMessage(String writerName, String content) {
-        com.ll.chat_2024_06_03.domain.chat.chatRoom.entity.ChatMessage chatMessage = com.ll.chat_2024_06_03.domain.chat.chatRoom.entity.ChatMessage
+        ChatMessage chatMessage = ChatMessage
                 .builder()
                 .chatRoom(this)
                 .writerName(writerName)
---
+++ src/main/java/com/ll/chat_2024_06_03/global/jpa/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.ll.chat_2024_06_03.global.jpa;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@MappedSuperclass
+@SuperBuilder
+@Getter
+@ToString
+@EqualsAndHashCode
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+}
